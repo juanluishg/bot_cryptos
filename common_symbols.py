f5f645b4204a6d996b64fdf2a7c9c81eed4a8d2b
Juan Luis <juanluishg@hotmail.es>
2020-03-15 20:50:08 +0100

    colors va regu

--- common_symbols.py
+++ common_symbols.py
@@ -1,16 +1,27 @@
 import ccxt
 import sys
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 if len(sys.argv) > 2:
     ids = list(sys.argv[1:])
     exchanges = {}
     print("Exchanges selected: ", ids)
-    string = ' {:<15} | '.format("Exchanges")
+    string = bcolors.BOLD + ' {:<15} | '.format("Exchanges")
     for id in ids:
         exchange = getattr(ccxt, id)()
         exchanges[id] = exchange
         markets = exchange.load_markets()
-        string += ' {:<15} | '.format(exchanges[id].name)
+        string += bcolors.BOLD + ' {:<15} | '.format(exchanges[id].name)
+        string += bcolors.BOLD + ' {:<15} | '.format(exchanges[id].name)
 
     allSymbols = [symbol for id in ids for symbol in exchanges[id].symbols]
     uniqueSymbols = list(set(allSymbols))
@@ -25,12 +36,13 @@ if len(sys.argv) > 2:
 
     try:
         for sym in arbitrableSymbols:
-            string = ' {:<15} | '.format(sym)
+            string = bcolors.HEADER + ' {:<15} | '.format(sym)
             for id in ids:
                 if sym in exchanges[id].symbols:
                     orderbook = exchanges[id].fetch_order_book(sym)
                     if (len(orderbook['bids']) > 0):
-                        string += ' {:<15} | '.format(orderbook['bids'][0][0])
+                        string += bcolors.OKGREEN + ' {:<15} | '.format(orderbook['bids'][0][0])
+                        string += bcolors.FAIL + ' {:<15} | '.format(orderbook['asks'][0][0])
                     else:
                         string += ' {:<15} | '.format('')
                 else:
@@ -38,12 +50,3 @@ if len(sys.argv) > 2:
             print(string)
     except:
         print(sym)
-
-    """"
-    for symbol in arbitrableSymbols:
-        string = ' {:<15} | '.format(symbol)
-        row = {}
-        for id in ids:
-            string += ' {:<15} | '.format(id if symbol in exchanges[id].symbols else '')
-        print(string)
-    """
