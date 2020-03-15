e8af2d84150b741b09b7042f935234a977218e52
Juan Luis <juanluishg@hotmail.es>
2020-03-15 20:37:03 +0100

    format

--- common_symbols.py
+++ common_symbols.py
@@ -4,22 +4,23 @@ import sys
 if len(sys.argv) > 2:
     ids = list(sys.argv[1:])
     exchanges = {}
-    print ("Exchanges selected: ", ids)
+    print("Exchanges selected: ", ids)
     string = ' {:<15} | '.format("Exchanges")
     for id in ids:
         exchange = getattr(ccxt, id)()
         exchanges[id] = exchange
         markets = exchange.load_markets()
         string += ' {:<15} | '.format(exchanges[id].name)
-        
+
     allSymbols = [symbol for id in ids for symbol in exchanges[id].symbols]
     uniqueSymbols = list(set(allSymbols))
-    arbitrableSymbols = sorted([symbol for symbol in uniqueSymbols if allSymbols.count(symbol) > 1])
+    arbitrableSymbols = sorted(
+        [symbol for symbol in uniqueSymbols if allSymbols.count(symbol) > 1])
 
     table = []
 
     print(string)
-    
+
     sym = ""
 
     try:
@@ -28,16 +29,16 @@ if len(sys.argv) > 2:
             for id in ids:
                 if sym in exchanges[id].symbols:
                     orderbook = exchanges[id].fetch_order_book(sym)
-                    if (len(orderbook['bids']) > 0): 
+                    if (len(orderbook['bids']) > 0):
                         string += ' {:<15} | '.format(orderbook['bids'][0][0])
-                    else: 
+                    else:
                         string += ' {:<15} | '.format('')
                 else:
                     string += ' {:<15} | '.format('')
             print(string)
     except:
         print(sym)
-    
+
     """"
     for symbol in arbitrableSymbols:
         string = ' {:<15} | '.format(symbol)
